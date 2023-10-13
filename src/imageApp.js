0d7387717e6e116558a3cd8736e4ed4c2a5ddaf2
YuraIvashkiv <113311064+YuraIvashkiv@users.noreply.github.com>
2023-10-13 19:56:24 +0300

    rename ImageApp

--- src/imageApp.js
+++ src/imageApp.js
@@ -1,45 +1,42 @@
-const { useState, useEffect } = require("react");
+const { useState, useEffect } = require('react');
 
-const imageApp = () => {
-    
-    const [query, setQuery] = useState('');
-    const [image, setImages] = useState([]);
-    const [page, setPage] = useState(1);
+const ImageApp = () => {
+  const [query, setQuery] = useState('');
+  const [image, setImages] = useState([]);
+  const [page, setPage] = useState(1);
 
-    const changeQuery = newQuery => {
-        setQuery(`${Date.now()}/${newQuery}`);
-        setImages([]);
-        setPage(1);
-    };
+  const changeQuery = newQuery => {
+    setQuery(`${Date.now()}/${newQuery}`);
+    setImages([]);
+    setPage(1);
+  };
 
-    const handleLoadMore = () => {
-         setPage(prevState => prevState + 1);
-            };
-    
-    useEffect(() => {
-        if (query === '') return;
-                 console.log(
-           `HTTP-запит за ${query}, і page=${page}`
-         );
-    }, [query, page])
-      return (
-        <div>
-          <div>
-            <form
-              onSubmit={event => {
-                event.preventDefault();
-                changeQuery(event.target.elements.query.value);
-                event.target.reset();
-              }}
-            >
-              <input type="text" name="query" />
-              <button type="submit">Submit</button>
-            </form>
-          </div>
-          <div>Gallery</div>
-          <div>
-            <button onClick={handleLoadMore}>Load More</button>
-          </div>
-        </div>
-      );
-}
+  const handleLoadMore = () => {
+    setPage(prevState => prevState + 1);
+  };
+
+  useEffect(() => {
+    if (query === '') return;
+    console.log(`HTTP-запит за ${query}, і page=${page}`);
+  }, [query, page]);
+  return (
+    <div>
+      <div>
+        <form
+          onSubmit={event => {
+            event.preventDefault();
+            changeQuery(event.target.elements.query.value);
+            event.target.reset();
+          }}
+        >
+          <input type="text" name="query" />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      <div>Gallery</div>
+      <div>
+        <button onClick={handleLoadMore}>Load More</button>
+      </div>
+    </div>
+  );
+};

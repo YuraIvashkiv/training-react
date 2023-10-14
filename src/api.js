9948809eb564f9fd6fcb1aa93532d0dd68e12302
YuraIvashkiv <113311064+YuraIvashkiv@users.noreply.github.com>
2023-10-14 10:59:02 +0300

    change name api

--- src/api.js
+++ src/api.js
@@ -3,16 +3,16 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://650995c3f6553137159bb321.mockapi.io/';
 
 export const fetchQuizzes = async () => {
-    const response = await axios.get('/quizzes');
-    return response.data
+  const response = await axios.get('/quizzes');
+  return response.data;
 };
 
-export const deleteQuiz = async (quizId) => {
-    const response = await axios.delete(`/quizzes/${quizId}`);
-    return response.data
+export const deleteQuizApi = async quizId => {
+  const response = await axios.delete(`/quizzes/${quizId}`);
+  return response.data;
 };
 
-export const createQuiz = async (quiz) => {
-    const response = await axios.post('/quizzes', quiz);
-return response.data
-}
+export const createQuiz = async quiz => {
+  const response = await axios.post('/quizzes', quiz);
+  return response.data;
+};
